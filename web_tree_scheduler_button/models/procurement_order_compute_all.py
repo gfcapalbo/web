e21f073e850ae9dc005a1d5bff8e42752d0ef686
gfcapalbo <giovanni@therp.nl>
2015-11-13 16:57:46 +0100

    [ADD] module that adds a scheduler button to the treeview of stock-picking models

---
+++ web_tree_scheduler_button/models/procurement_order_compute_all.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, api
+from datetime import datetime as dt
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+
+class ProcurementOrderComputeAll(models.TransientModel):
+    _inherit = 'procurement.order.compute.all'
+
+    @api.multi
+    def procure_calculation(self):
+        res = super(ProcurementOrderComputeAll, self).procure_calculation()
+        self.env['ir.config_parameter'].set_param(
+            key='web_tree_scheduler_button.last_scheduler_update',
+            value=dt.strftime(dt.now(), DEFAULT_SERVER_DATETIME_FORMAT))
+        return res
---
+++ web_tree_scheduler_button/static/src/js/web_tree_scheduler_button.js
@@ -0,0 +1,75 @@
+//  @@@ web_export_view custom JS @@@
+//#############################################################################
+//    
+//    Copyright (C) 2012 Agile Business Group sagl (<http://www.agilebg.com>)
+//    Copyright (C) 2012 Therp BV (<http://therp.nl>)
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as published
+//    by the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//#############################################################################
+openerp.web_tree_button = function (instance) {
+
+    var _t = instance.web._t, QWeb = instance.web.qweb;
+    var last_update='';
+    
+    instance.web.View.include({
+        start: function () 
+        {
+            var self = this;
+            return this._super.apply(this, arguments)
+            .then(function()
+            {
+                if (self.ViewManager &&
+                        self.$buttons  &&
+                        self.model && self.model == 'stock.picking' &&
+                        self.ViewManager.active_view == 'list')
+                {
+                    self.$buttons.append(QWeb.render('AddTreeButton', {widget: self}));
+                    self.getLastUpdate();
+                    self.$buttons.find('.oe_extrabutton').on('click', self.proxy('perform_button_action'));
+                }
+            })
+        },
+        perform_button_action:  function() 
+        {
+            var self = this;
+            var upd = new instance.web.Model("procurement.order.compute.all");
+            self.waiting();
+            return upd.call("procure_calculation" ,  [[]], {})
+                .then(self.proxy('getLastUpdate'));
+        },
+
+        getLastUpdate: function()
+        {
+            var self = this,
+                ir_config_parameter = new openerp.web.Model('ir.config_parameter');
+            self.waiting();
+            return ir_config_parameter.call(
+                'get_param', ['web_tree_scheduler_button.last_scheduler_update'])
+                .then(function (date)
+                {
+                    var datestr = _.str.sprintf(
+                        _t('Last statuses Update: %s'),
+                        date);
+                    self.$buttons.find('#dateplace').html(datestr);
+                });
+        }, 
+        waiting: function()
+        {
+            var self = this;
+            self.$buttons.find('#dateplace').html('Updating ....');
+        }, 
+});
+
+}

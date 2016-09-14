46101e066c3abc8cc5fa465e35125268093c3916
gfcapalbo <giovanni@therp.nl>
2016-09-14 14:07:44 +0200

    Version to 7

--- web_advanced_search_x2x/__openerp__.py
+++ web_advanced_search_x2x/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Search x2x fields",
-    "version": "8.0.1.0.0",
+    "version": "7.0.1.0.0",
     "author": "Therp BV, "
               "Odoo Community Association (OCA)",
     "description": """
